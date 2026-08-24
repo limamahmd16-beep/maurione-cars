68a726a2efff98f6a7117135b8cc6c95d7550d6a
limamahmd16-beep <limamahmd16@gmail.com>
2026-08-25 00:01:36 +0100

    Make premium white theme the fixed default

--- src/theme-bootstrap.js
+++ src/theme-bootstrap.js
@@ -1,7 +1,7 @@
-const paletteVersion = 'maurione_cars_palette_v2';
+const paletteVersion = 'maurione_cars_palette_v3';
 try {
   if (!localStorage.getItem(paletteVersion)) {
-    localStorage.setItem('maurione_cars_theme', 'dark');
+    localStorage.setItem('maurione_cars_theme', 'light');
     localStorage.setItem(paletteVersion, '1');
   }
 } catch (_) {}
