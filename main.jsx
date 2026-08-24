336f36a63ad3a8b5c1f21155d171eccf32ef341b
limamahmd16-beep <limamahmd16@gmail.com>
2026-08-24 22:30:50 +0100

    Wire premium theme and mandatory user accounts

--- main.jsx
+++ main.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './src/theme-bootstrap.js';
 import App from './src/App.jsx';
+import UserGate from './src/UserGate.jsx';
 import './src/styles.css';
+import './src/premium.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <UserGate>
+      <App />
+    </UserGate>
   </React.StrictMode>
 );
