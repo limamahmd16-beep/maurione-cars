42a99971fa022a0efeb14218ab35f3d15a61b25c
limamahmd16-beep <limamahmd16@gmail.com>
2026-08-25 20:54:39 +0100

    Assemble exact approved welcome artwork

---
+++ src/welcome-approved-image.js
@@ -0,0 +1,16 @@
+import p1 from './welcome-data/p1.js';
+import p2 from './welcome-data/p2.js';
+import p3 from './welcome-data/p3.js';
+import p4 from './welcome-data/p4.js';
+import p5 from './welcome-data/p5.js';
+import p6a from './welcome-data/p6a.js';
+import p6b from './welcome-data/p6b.js';
+import p6c from './welcome-data/p6c.js';
+import p6d from './welcome-data/p6d.js';
+import p6e from './welcome-data/p6e.js';
+import p6f from './welcome-data/p6f.js';
+import p6g from './welcome-data/p6g.js';
+import p7a from './welcome-data/p7a.js';
+import p7b from './welcome-data/p7b.js';
+
+export default `data:image/webp;base64,${p1}${p2}${p3}${p4}${p5}${p6a}${p6b}${p6c}${p6d}${p6e}${p6f}${p6g}${p7a}${p7b}`;
