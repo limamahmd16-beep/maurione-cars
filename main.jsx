1183e0ea7692114a419adf553d2d2952176b6e39
limamahmd16-beep <limamahmd16@gmail.com>
2026-08-25 13:37:00 +0100

    Activate exact approved MauriOne mobile interface

--- main.jsx
+++ main.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './src/theme-bootstrap.js';
-import App from './src/App.jsx';
+import AppExact from './src/AppExact.jsx';
 import UserGate from './src/UserGate.jsx';
 import './src/styles.css';
 import './src/premium.css';
 import './src/brand-fix.css';
+import './src/exact.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <UserGate>
-      <App />
+      <AppExact />
     </UserGate>
   </React.StrictMode>
 );
