45844404758c845b0b1e0ed350f2ab8c58417403
limamahmd16-beep <limamahmd16@gmail.com>
2026-08-25 14:07:21 +0100

    Activate functional controls across MauriOne Cars

--- main.jsx
+++ main.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './src/theme-bootstrap.js';
 import AppExact from './src/AppExact.jsx';
+import FunctionalEnhancer from './src/functional-enhancer.jsx';
 import UserGate from './src/UserGate.jsx';
 import './src/styles.css';
 import './src/premium.css';
 import './src/brand-fix.css';
 import './src/exact.css';
 import './src/no-messages.css';
+import './src/functional-enhancer.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <UserGate>
       <AppExact />
+      <FunctionalEnhancer />
     </UserGate>
   </React.StrictMode>
 );
